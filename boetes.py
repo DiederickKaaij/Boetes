bbe7c63a114ab731b7d0c8b3fd4976380924cddf
Diederick Kaaij <diederickkaaij@gmail.com>
2019-09-17 09:33:31 +0200

    Add constant variables

--- boetes.py
+++ boetes.py
@@ -1,11 +1,14 @@
+MIN_SPEED = 50
+MAX_SPEED = 80
+
 def hoogteBoete(snelheid):
     
-    if snelheid < 55:
+    if snelheid < (MIN_SPEED + 5):
         return "OK"
-    elif snelheid > 80:
+    elif snelheid > MAX_SPEED:
         return "Rijbewijs ingenomen"
     else:
-        snelheid -= 50
+        snelheid -= MIN_SPEED
         boete = int(snelheid / 5)
         boete = boete * 10
         return boete
